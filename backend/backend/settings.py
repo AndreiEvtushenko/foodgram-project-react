5779be5981292c8bf986f7b30397a8105821ecdb
root <root@HOME-PC>
2023-08-28 15:41:35 +0300

    Settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 6,
 }
 
 LANGUAGE_CODE = 'en-us'
