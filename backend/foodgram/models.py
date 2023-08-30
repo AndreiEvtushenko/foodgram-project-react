f0cd90646da0a022cdee80f47723f2dc7880bb63
root <root@HOME-PC>
2023-08-30 17:39:50 +0300

    Models Recipe

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -212,6 +212,12 @@ class Recipe(models.Model):
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
     
+    def save_model(self, request, obj, form, change):
+        if not obj.tags.exists() or not obj.ingredients.exists():
+            raise ValidationError('Необходимо выбрать минимум один тег и один ингредиент.')
+        
+        super().save_model(request, obj, form, change)
+    
     def clean(self):
         if not self.tags.exists() or not self.ingredients.exists():
             raise ValidationError('Необходимо выбрать минимум один тег и один ингредиент.')
